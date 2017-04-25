bf9a93408c48da2eef327edb6b048d75d325e085
izardj <izard.jerome@free.fr>
2017-04-25 16:47:27 +0200

    Correction import

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -4,8 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
-import com.mysql.jdbc.Statement;
+import java.sql.Statement;
 
 import metier.Client;
 
